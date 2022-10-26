b48ab16c43ec187cd3c24acb28180b5d7f836e5b
K. E. Aydın <79324596+kaan-master@users.noreply.github.com>
2022-10-26 17:09:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,123 @@
+let result = document.getElementById("result").innerHTML = "";
+ 
+function One()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "1" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+
+function Two()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "2" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+
+function Three()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "3" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+
+
+function Four()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "4" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+
+function Five()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "5" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+function Six()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "6" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+function Seven()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "7" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+function Eight()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "8" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+function Nine()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "9" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+
+function Zero()
+{ 
+    if (result = !document.getElementById("result").innerHTML < 1){
+        let result = document.getElementById("result").innerHTML += "0" ;
+        console.log("True")
+    } 
+        else{ 
+            let result = document.getElementById("result").innerHTML = 1 ;
+            console.log("False")
+                }
+}
+
+function reset()
+{ 
+    let result = document.getElementById("result").innerHTML = "";
+}
